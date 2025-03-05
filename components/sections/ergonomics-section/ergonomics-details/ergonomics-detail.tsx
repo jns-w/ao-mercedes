f1f53e833b2588577c07abc5bf0a8f4424d51f53
jns-w <jns.w@icloud.com>
2025-03-06 04:41:23 +0800

    ergonomics section clean up

--- components/sections/ergonomics-section/ergonomics-details/ergonomics-detail.tsx
+++ components/sections/ergonomics-section/ergonomics-details/ergonomics-detail.tsx
@@ -4,6 +4,7 @@ import style from "./ergonomics-detail.module.scss";
 import type { ErgonomicsDetail } from "@/components/sections/ergonomics-section/ergonomics-section";
 import clsx from "clsx";
 import { BubbleCard } from "@/components/cards/bubble-card/bubble-card";
+import { motion } from "motion/react";
 
 interface ErgonomicsDetailProps extends ErgonomicsDetail {
   anchor: "left" | "right";
@@ -11,17 +12,19 @@ interface ErgonomicsDetailProps extends ErgonomicsDetail {
 
 export function ErgonomicsDetail(props: ErgonomicsDetailProps) {
   return (
-    <div className={clsx(style.ergonomicsDetailDiv, props.anchor === "left" ? style.leftAnchor : style.rightAnchor)}>
+    <motion.div
+      className={clsx(style.ergonomicsDetailDiv, props.anchor === "left" ? style.leftAnchor : style.rightAnchor)}
+      initial={{ x: props.anchor === "left" ? 200 : -200, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      transition={{ duration: 0.3, delay: .1, ease: "easeOut" }}
+    >
       <div className={style.ergonomicsDetailImageDiv}>
-        <Image src={props.imageSrc} alt="" width={200} height={200}/>
-        <BubbleCard className={style.descriptionCard}
-                    text={
-                      <>
-                        <p>{props.description}</p>
-                        <p>{props.photoCredit}</p>
-                      </>
-                    }/>
+        <Image src={props.imageSrc} alt="" width={250} height={250}/>
+        {/*<BubbleCard className={style.descriptionCard}>*/}
+        {/*  <p>{props.description}</p>*/}
+        {/*  <p>{props.photoCredit}</p>*/}
+        {/*</BubbleCard>*/}
       </div>
-    </div>
+    </motion.div>
   )
 }
